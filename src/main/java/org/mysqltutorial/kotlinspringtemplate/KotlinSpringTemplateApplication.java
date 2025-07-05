c7eb26f12737bf413b4bde7d9735dab895bf4ebe
Jason <extremewinds@gmail.com>
2025-07-05 17:49:18 +0200

    Initial commit

---
+++ src/main/java/org/mysqltutorial/kotlinspringtemplate/KotlinSpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.mysqltutorial.kotlinspringtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinSpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinSpringTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mysqltutorial/kotlinspringtemplate/KotlinSpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mysqltutorial.kotlinspringtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinSpringTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
