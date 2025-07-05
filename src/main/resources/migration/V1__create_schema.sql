38da498ed7f4ab60ba7d627d6509e96126d46e34
Jason <extremewinds@gmail.com>
2025-07-05 18:32:02 +0200

    make frontend work

---
+++ src/main/resources/migration/V1__create_schema.sql
@@ -0,0 +1,11 @@
+/*
+  Name: MySQL Sample Database classicmodels
+  Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
+*/
+
+
+/* Create the database */
+CREATE DATABASE  IF NOT EXISTS classicmodels;
+
+/* Switch to the classicmodels database */
+USE classicmodels;
