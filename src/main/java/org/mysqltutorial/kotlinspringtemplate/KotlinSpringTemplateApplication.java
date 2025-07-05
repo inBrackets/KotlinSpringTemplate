6d74f1cfa6d230415bdb42dc0e4bd181f2359508
Jason <extremewinds@gmail.com>
2025-07-05 17:50:04 +0200

    first commit

--- src/main/java/org/mysqltutorial/kotlinspringtemplate/KotlinSpringTemplateApplication.java
+++ src/main/java/org/mysqltutorial/kotlinspringtemplate/KotlinSpringTemplateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KotlinSpringTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KotlinSpringTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KotlinSpringTemplateApplication.class, args);
+    }
 
 }
